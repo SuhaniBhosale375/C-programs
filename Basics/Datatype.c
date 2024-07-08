a9dff36e421237d062d13991c6caa255f383c591
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-07-09 00:34:41 +0530

    Basic Program

---
+++ Basics/Datatype.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int R_No   = 77;
+	char Name  = 'S';
+	float Per  = 94.67;
+	double d   = 10.5;
+	
+	printf("value of integer -> %d\n",R_No);
+	printf("value of Character -> %c\n",Name);
+	printf("value of float -> %f\n",Per);
+	printf("value of double -> %lf\n",d);
+	
+	return 0;
+	
+}
