c20fb14deb7504b536f6a65dc901d3c56251dc3b
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-07-08 15:24:30 +0530

    My Project

---
+++ Basics/First.c
@@ -0,0 +1,21 @@
+#include<stdio.h>   // Header file inclusion         
+
+int main()         // Enty Point function
+{                  // Block start
+
+	printf("\n First C Program");      // Display the data on the string
+	
+	return 0;
+}                    //Block end              
+
+
+// Step1 : Compile the code
+// gcc First.c -o Myexe
+// gcc  -> GNU C Compiler
+// First.c  -> File name
+// -o   -> Output flag
+// Myexe    -> Name of executable
+
+// Step2 : Execute the code
+// Myexe        -> On windows
+// ./Myexe      -> On linux / Macos
