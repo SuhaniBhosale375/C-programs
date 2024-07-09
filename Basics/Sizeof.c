5fba6ff188695d65d807fe011cbcdd6571a57be6
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-07-09 22:57:17 +0530

    Basic Program

---
+++ Basics/Sizeof.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int No = 0;
+	char Ch = '\0';
+	float f = 0.0;
+	double d = 0.0;
+	
+	printf("Size of integer : %d\n",sizeof(No));
+	printf("Size of Character: %d\n",sizeof(Ch));
+	printf("Size of Float : %d\n",sizeof(f));
+	printf("Size of Double: %d\n",sizeof(d));
+	
+	
+	return 0;
+}
