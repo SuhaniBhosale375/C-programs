56a48103a111927c4a2eb49f89dd587894910276
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-07-22 16:00:57 +0530

    C Programming

---
+++ Number Programs/Display Non Factors.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void DisplayNonFactors(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt) != 0)
+        {
+            printf("%d\n",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+  
+    printf("Enter the number \n");
+    scanf("%d",&iValue);
+
+    DisplayNonFactors(iValue);
+   
+    return 0;
+}
