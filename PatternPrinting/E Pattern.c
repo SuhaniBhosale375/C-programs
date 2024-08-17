2f1ec1b451182aca81f344d3d2e5fe21517a5ec6
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-08-17 21:33:59 +0530

    C Programming

---
+++ PatternPrinting/E Pattern.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int R = 0, C = 0;
+    int i = 0,j = 0;
+
+    printf("Enter Row Number :\n");
+    scanf("%d",&i);
+
+    printf("Enter Column Number :\n");
+    scanf("%d",&j);
+
+    for(R = 1; R <= i; R++)
+    {
+        for(C = 1; C<= j; C++)
+        {
+           if(R == 1||C == 1 || R == j || (R == (j/2) + 1) && (R >= C))
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+}
