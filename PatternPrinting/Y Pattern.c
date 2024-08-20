d3d21c01ab50f8a71c479b85d19e61a8a159ff8a
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-08-20 21:43:55 +0530

    Pattern

---
+++ PatternPrinting/Y Pattern.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int R = 0, C = 0;
+    int i = 0,j = 0;
+
+    printf("Enter Row Number :\n");
+    scanf("%d",&i);
+
+    printf("Enter Column Number :\n");
+    scanf("%d",&j);
+
+    for(R = 1; R <= i; R++)
+    {
+        for(C = 1; C<= j; C++)
+        {
+            if(((R == C) && (C < (j/2+1))) || (((R + C == i + 1) && (C >= (j/2+1)))) || ((C == (j/2+1))&& (R > (j/2+1))))
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+}
