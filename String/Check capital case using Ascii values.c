b6ca21da3e150ebddc72281a76c7016144646e83
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-10-05 07:27:47 +0530

    C Programming

---
+++ String/Check capital case using Ascii values.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool IsCapital(char ch)
+{
+    if((ch >=  65) &&(ch <= 90))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    bool bRet =  false;
+
+    printf("Please enter one character\n");
+    scanf("%c",&cValue);
+
+    bRet = IsCapital(cValue);
+
+    if(bRet == true)
+    {
+        printf("%c is a capital latter\n",cValue);
+    }
+    else
+    {
+        printf("%c is not a Capital latter\n",cValue);
+    }
+
+    return 0;
+}
