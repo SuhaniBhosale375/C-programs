14a9fad90670fcde2046bd13e4236a057fcc0f28
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-01-06 17:38:07 +0530

    C Programming

---
+++ PatternPrinting/06_Accept number of rows and column and display pattern.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+/*
+    Row     4
+    Coloum  4
+
+    1   $   3   $
+    1   $   3   $
+    1   $   3   $
+    1   $   3   $
+   
+*/
+
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {    for(j = 1; j <= iCol; j++)
+        {
+            if((j % 2) == 0)
+            {
+                printf("$\t");
+
+            }
+            else
+            {
+                printf("%d\t",j);
+            }
+        }
+        printf("\n"); 
+    }    
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("Enter number of Rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of colums  :\n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
