38b659bd459934013ca7cd5e6e43b2948cdffc06
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-02-08 20:22:19 +0530

    C Programming

---
+++ PatternPrinting/Diamond.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0,j = 0,k = 0;
+
+    int R = 0;
+
+    printf("Enter number of rows ");
+    scanf("%d",&R);
+
+    for(i = 1; i <= R; i++)
+    {
+        for(j = 1; j <= R - i; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1; k <= (2 * i - 1); k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for(i = R - 1; i >= 1; i--)
+    {
+        for(j = 1; j <= R - i; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1; k <= (2 * i - 1); k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
