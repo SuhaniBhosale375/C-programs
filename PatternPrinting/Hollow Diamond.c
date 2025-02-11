79680494998ff6414bec6b8a211399b1f155af55
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-02-11 22:38:15 +0530

    C Programming

---
+++ PatternPrinting/Hollow Diamond.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0,j = 0,k = 0;
+
+    int R = 0;
+
+    printf("Enter number of rows ");
+    scanf("%d",&R);
+
+    for(i = 1; i < R - 1; i++)
+    {
+        for(j = 1; j <= R - i; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1; k <= (2 * i - 1); k++)
+        {
+            if((k == 1) ||(k == 2 * i - 1 || (i == R)))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
+    for(i = R - 1; i >= 1; i--)
+    {
+        for(j = 1; j <= R - i; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1; k <= (2 * i - 1); k++)
+        {
+            if((k == 1) ||(k == 2 * i - 1 || (i == R)))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+            
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
