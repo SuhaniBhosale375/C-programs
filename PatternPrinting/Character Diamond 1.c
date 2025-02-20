fa6b93539239b866bd7a03588a8c42ab3b4c3a64
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-02-20 17:24:19 +0530

    C Programs

---
+++ PatternPrinting/Character Diamond 1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0,j = 0,k = 0;
+
+    int R = 0;
+    char ch = 'A';
+
+    printf("Enter number of rows ");
+    scanf("%d",&R);
+
+    for(i = 1; i <= R; i++)
+    {
+        for(j = 1; j <= R - i; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1,ch = 'A'; k <= (2 * i - 1); k++,ch++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    
+    for(i = R - 1; i >= 1; i--)
+    {
+        for(j = 1; j <= R - i; j++)
+        {
+            printf(" ");
+        }
+        for(k = 1,ch = 'A'; k <= (2 * i - 1); k++,ch++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
